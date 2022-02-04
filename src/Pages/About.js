f5f293602dc42c5ad044ce99702673825a090d20
Kevin West <kevinwestnyc@gmail.com>
2022-02-04 18:32:05 -0500

    added Typescript icon

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -6,20 +6,21 @@ import scale from "../Images/scale.png"
 export default function About() {
   return (
     <Container className="page-content">
-      <h1 className="page-title" >About</h1>
+      <h1 className="page-title">About</h1>
       <Grid container>
         <Grid container direction="row">
           <Grid item id="about-text" direction="column" xs={12} sm={6}>
             <p className="montserrat">
-              Hi, I'm Kevin! I'm a self-taught programmer from Brooklyn, NY.
-              A little over one year ago I decided to change careers from an athletic
-              trainer for kids to a full-stack developer and it has been an exciting
-              journey. In that time I have absorbed new languages, new ways of
-              thinking, and created a number of fun and challenging projects. I
-              am currently working at Nifty Art Collective, a Web3 startup,
-              building front-end React components as well as components that
-              link to the blockchain. I'm excited to continue my growth and
-              build interesting projects with cool people!
+              Hi, I'm Kevin! I'm a self-taught programmer from Brooklyn, NY. A
+              little over one year ago I decided to change careers from an
+              athletic trainer for kids to a full-stack developer and it has
+              been an exciting journey. In that time I have absorbed new
+              languages, new ways of thinking, and created a number of fun and
+              challenging projects. I am currently working at Nifty Art
+              Collective, a Web3 startup, building front-end React components as
+              well as components that link to the blockchain. I'm excited to
+              continue my growth and build interesting projects with cool
+              people!
             </p>
             <Hidden smUp>
               <Grid
@@ -33,7 +34,9 @@ export default function About() {
               </Grid>
             </Hidden>
             <Grid id="language-container" direction="row">
-              <Typography id="language-title">I have experience with:</Typography>
+              <Typography id="language-title">
+                I have experience with:
+              </Typography>
               <img
                 id="scale"
                 className="ten"
@@ -52,12 +55,12 @@ export default function About() {
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
                 title="CSS"
               />
-                <img
-                  id="materialui-icon"
-                  className="icon-image eight"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg"
-                  title="MaterialUI"
-                />
+              <img
+                id="materialui-icon"
+                className="icon-image eight"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg"
+                title="MaterialUI"
+              />
               <img
                 id="js-icon"
                 className="icon-image seven"
@@ -70,12 +73,12 @@ export default function About() {
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
                 title="React"
               />
-                <img
-                  id="github-icon"
-                  className="icon-image six"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
-                  title="Github"
-                />
+              <img
+                id="github-icon"
+                className="icon-image six"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                title="Github"
+              />
               <img
                 id="bootstrap-icon"
                 className="icon-image five"
@@ -100,6 +103,13 @@ export default function About() {
                 src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-original.svg"
                 title="Ruby"
               />
+              <img 
+                id="typescript-icon"
+                className="icon-image four"
+                src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+                title="Typescript"
+                />
+
               <img
                 id="heroku-icon"
                 className="icon-image four"
